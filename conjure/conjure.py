ed3b3a5694e9d9bfe33939da237f01ab08b47b45
Özgür Akgün <ozgurakgun@gmail.com>
2022-04-15 22:26:09 +0100

    Use  --solver=chuffed

--- conjure/conjure.py
+++ conjure/conjure.py
@@ -16,9 +16,9 @@ class Conjure:
                 else:
                     raise Exception("Variable {0} is not defined".format(p))
             temp_params_file = conjurehelper.create_params_file(params)
-            shell_output = Popen(["conjure solve -ac " + temp_essence_file + " " + temp_params_file +" --output-format=json", ], shell=True, stdout=PIPE, stderr=PIPE)
+            shell_output = Popen(["conjure solve -ac " + temp_essence_file + " " + temp_params_file +" --output-format=json --solver=chuffed", ], shell=True, stdout=PIPE, stderr=PIPE)
         else:
-            shell_output = Popen(["conjure solve -ac " + temp_essence_file + " --output-format=json", ], shell=True, stdout=PIPE, stderr=PIPE)
+            shell_output = Popen(["conjure solve -ac " + temp_essence_file + " --output-format=json --solver=chuffed", ], shell=True, stdout=PIPE, stderr=PIPE)
 
         _, error = shell_output.communicate()
         if(error):
