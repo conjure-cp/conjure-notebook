b6d119828d427501984a33027dedea3e7b53010b
Ogabek Yusupov <ogabekyusupov@gmail.com>
2022-04-23 22:01:47 +0100

    incremental model building

--- conjure/ConjureMagics.py
+++ conjure/ConjureMagics.py
@@ -1,17 +1,32 @@
 import sys
 from .conjure import Conjure
-from IPython.core.magic import (Magics, magics_class, line_cell_magic)
+from IPython.core.magic import (Magics, magics_class, line_cell_magic, line_magic)
 
 @magics_class
 class ConjureMagics(Magics):
-        @line_cell_magic
-        def conjure(self, args, code):
+     conjure_model = ""
+
+     @line_cell_magic
+     def conjure(self, args, code):
           conjure = Conjure()
+          conjure_model_pre = ""
           try:
-               resultdict = conjure.solve(args, code, dict(self.shell.user_ns))
+               conjure_model_pre = self.conjure_model
+               self.conjure_model += code
+               resultdict = conjure.solve(args, self.conjure_model, dict(self.shell.user_ns))
           except Exception as e:
-             print("{}: {}".format(type(e).__name__, e), file=sys.stderr)
-             return;
+               self.conjure_model = conjure_model_pre
+               print("{}: {}".format(type(e).__name__, e), file=sys.stderr)
+               return;
           for key, value in resultdict.items():
                self.shell.user_ns[key] = value
           return resultdict
+
+     @line_magic
+     def clear_conjure(self, line):
+          self.conjure_model = ""
+          print('Conjure model cleared')
+
+     @line_magic
+     def print_conjure(self, line):
+          print(self.conjure_model)
