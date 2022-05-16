6c7065a0a44662f73b771829028e4efafc1ae754
Ogabek Yusupov <ogabekyusupov@gmail.com>
2022-05-16 18:11:43 +0100

    added additional solvers

--- conjure/ConjureMagics.py
+++ conjure/ConjureMagics.py
@@ -7,11 +7,14 @@ from .conjure import Conjure
 
 @magics_class
 class ConjureMagics(Magics):
+
      conjure_models = []
      print_output = 'Yes'
-     conjure_solvers = ['chuffed', 'minion', 'glucose', 'glucose-syrup', 
-     'lingeling', 'open-wbo']
+     conjure_solvers = ['minion', 'gecode', 'chuffed', 'glucose', 'glucose-syrup',
+      'lingeling', 'cadical', 'kissat', 'minisat', 'bc_minisat_all', 'nbc_minisat_all',
+      'open-wbo', 'coin-or', 'cplex', 'boolector', 'yices', 'z3']
      selected_solver = 'chuffed'
+
      @line_cell_magic
      def conjure(self, args, code):
           conjure = Conjure()
@@ -58,6 +61,10 @@ class ConjureMagics(Magics):
           #      display(rbtn)
           # display(btn)
           args = ' --solver=' + self.selected_solver + ' ' +  args
+
+          # some solvers works only when --number-of-solutions=all
+          if self.selected_solver == 'bc_minisat_all' or self.selected_solver == 'nbc_minisat_all':
+               args+=' --number-of-solutions=all'               
 
           try:
                self.conjure_models.append(code)
@@ -130,22 +137,8 @@ class ConjureMagics(Magics):
 
      @line_magic
      def conjure_help(self, line):
-          if line == 'conjure':
-               print("Usage example for %%conjure:")
-               print("""
-%%conjure
-letting letters be new type enum {S,E,N,D,M,O,R,Y}
-find f : function (injective) letters --> int(0..9)
-such that
-               1000 * f(S) + 100 * f(E) + 10 * f(N) + f(D) +
-               1000 * f(M) + 100 * f(O) + 10 * f(R) + f(E) =
-10000 * f(M) + 1000 * f(O) + 100 * f(N) + 10 * f(E) + f(Y)
-
-such that f(S) > 0, f(M) > 0
-               """)
-               return
           help_str = "Conjure jupyter extension magic commands: \n"
-          help_str+= "%%conjure - Runs the provided conjure model along with previously ran models. \nFor usage example run: %conjure_help conjure\n"
+          help_str+= "%%conjure - Runs the provided conjure model along with previously ran models."
           help_str+= "%conjure_clear - clears the previously ran conjure models.\n"
           help_str+= "%conjure_print - prints the previously ran conjure models.\n"
           help_str+="%conjure_rollback - removes the last appended conjure model.\n"
--- installcolab.sh
+++ installcolab.sh
@@ -5,8 +5,8 @@ rm -rf conjure-notebook-executable
 git clone https://github.com/ogabek96/conjure-notebook-executable.git && cd "$(basename "$_" .git)"
 
 # copy the executables to /usr/local/bin
-chmod +x conjure savilerow savilerow.jar bin/*
-cp -R conjure savilerow savilerow.jar bin/* /usr/local/bin
+chmod +x conjure savilerow savilerow.jar solvers/*
+cp -R conjure savilerow savilerow.jar solvers/* /usr/local/bin
 
 # installing the conjure extension
 pip install git+https://github.com/conjure-cp/conjure-notebook.git
