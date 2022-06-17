1fc73222bb8e41de4c748df638c7090852aa1b6c
Özgür Akgün <ozgurakgun@gmail.com>
2022-06-17 16:22:14 +0200

    remove sample_data

--- installcolab.sh
+++ installcolab.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # remove the sample-data directory that google colab creates, we don't need them
-rm -rf sample-data
+rm -rf sample_data
 
 # download latest release from Github
 rm -rf conjure-notebook-executable
