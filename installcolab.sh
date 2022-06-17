e84d3df4bd3ffc0a1a6b9aeb382a71d6837eb6a6
Özgür Akgün <ozgurakgun@gmail.com>
2022-06-17 16:06:51 +0200

    fiddle with the installing script (how we do cd)

--- installcolab.sh
+++ installcolab.sh
@@ -5,11 +5,14 @@ rm -rf sample-data
 
 # download latest release from Github
 rm -rf conjure-notebook-executable
-git clone --quiet https://github.com/ogabek96/conjure-notebook-executable.git && cd "$(basename "$_" .git)"
+git clone --quiet https://github.com/ogabek96/conjure-notebook-executable.git
 
+(
+cd conjure-notebook-executable
 # copy the executables to /usr/local/bin
 chmod +x conjure savilerow savilerow.jar solvers/*
 cp -R conjure savilerow savilerow.jar solvers/* /usr/local/bin
+)
 
 # we don't need to keep these around any more
 rm -rf conjure-notebook-executable
