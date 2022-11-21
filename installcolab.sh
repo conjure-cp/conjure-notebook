4da815a0a70e36d94b84f44357fefb71384efa44
Özgür Akgün <ozgurakgun@gmail.com>
2022-11-21 11:54:28 +0000

    Filename change

--- installcolab.sh
+++ installcolab.sh
@@ -6,12 +6,12 @@ echo "Installing Conjure..."
 rm -rf sample_data
 
 # download latest release from Github
-wget --quiet --no-check-certificate -c https://github.com/conjure-cp/conjure/releases/download/v2.4.0/conjure-v2.4.0-linux-solvers.zip
-unzip -q -o conjure-v2.4.0-linux-solvers.zip
-cp -r conjure-v2.4.0-linux-solvers/* /usr/local/bin
+wget --quiet --no-check-certificate -c https://github.com/conjure-cp/conjure/releases/download/v2.4.0/conjure-v2.4.0-linux-with-solvers.zip
+unzip -q -o conjure-v2.4.0-linux-with-solvers.zip
+cp -r conjure-v2.4.0-linux-with-solvers/* /usr/local/bin
 
 # we don't need to keep these around any more
-rm -rf conjure-v2.4.0-linux-solvers conjure-v2.4.0-linux-solvers.zip
+rm -rf conjure-v2.4.0-linux-with-solvers conjure-v2.4.0-linux-with-solvers.zip
 
 # installing the conjure extension
 pip --quiet install git+https://github.com/conjure-cp/conjure-notebook.git > /dev/null
