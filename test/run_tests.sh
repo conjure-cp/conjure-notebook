870680c794d61610adc2b231d71cfb9dc5211c06
Özgür Akgün <ozgurakgun@gmail.com>
2023-08-08 19:23:46 +0100

    Use jq to remove the metadata

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -5,10 +5,14 @@ set -eux
 if [ $# -eq 0 ]; then
     for f in *.ipynb; do
         jupyter nbconvert --output-dir=outputs --to notebook --execute $f
+        jq '.cells[].metadata = {}' outputs/$f > outputs/$f.tmp
+        mv outputs/$f.tmp outputs/$f
     done
 else
     for f in "$@"; do
         jupyter nbconvert --output-dir=outputs --to notebook --execute $f
+        jq '.cells[].metadata = {}' outputs/$f > outputs/$f.tmp
+        mv outputs/$f.tmp outputs/$f
     done
 fi
 
