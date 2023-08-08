83f370f272ba3f776478ec9d176f25ea5ea289ca
Özgür Akgün <ozgurakgun@gmail.com>
2023-08-08 16:45:32 +0100

    update tests to not use conjure_clear + output notebooks

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -4,14 +4,12 @@ set -eux
 
 if [ $# -eq 0 ]; then
     for f in *.ipynb; do
-        jupyter nbconvert --to markdown --execute $f
+        jupyter nbconvert --output-dir=outputs --to notebook --execute $f
     done
 else
-    for f in "$@"
-    do
-        jupyter nbconvert --to markdown --execute $f
+    for f in "$@"; do
+        jupyter nbconvert --output-dir=outputs --to notebook --execute $f
     done
 fi
-
 
 git diff --exit-code .
