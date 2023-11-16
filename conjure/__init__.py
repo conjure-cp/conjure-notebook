b593febc7122fea14f54e4fb809ce31d987f1e00
Özgür Akgün <ozgurakgun@gmail.com>
2023-11-16 22:04:57 +0000

    fiddle

--- conjure/__init__.py
+++ conjure/__init__.py
@@ -1,7 +1,8 @@
 from .conjuremagics import ConjureMagics
 from .conjure import Conjure
 from os import path
-from IPython.core.display import display, Javascript
+from IPython.display import display, Markdown, Javascript
+
 
 with open(path.join(path.abspath(path.dirname(__file__)), 'static/syntax-highlight.js')) as f:
     initHighlighter = f.read()
@@ -11,4 +12,4 @@ def load_ipython_extension(ipython):
     display(Javascript(initHighlighter))
     if Conjure.check_conjure():  # check conjure is installed
         ipython.register_magics(ConjureMagics)
-        print('Conjure extension is loaded - run: %conjure_help')
+        display(Markdown('Conjure extension is loaded - run `%conjure_help`'))
